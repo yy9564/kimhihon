5d2cd0d5db8cffa104ec4b182bc4e045466fbf43
heo <heo9564@naver.com>
2018-04-11 21:40:06 +0900

    프로젝트 생성

---
+++ yeah/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+    public GameObject player;
+    //private Vector2 velocity;
+    //public float smoothTimeX;
+
+	// Use this for initialization
+	void Start () {
+        //player = GameObject.FindGameObjectWithTag
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float posX = player.transform.position.x;
+
+        transform.position = new Vector3(posX, 0, -10);
+	}
+}
---
+++ yeah/Assets/Scripts/PlayerMoveScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMoveScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision) {
+        
+    }
+    private void OnTriggerStay2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Door") {
+            if (Input.GetKey(KeyCode.UpArrow)) {
+                SceneManager.LoadScene("_Complete-Game");
+            }
+
+        }
+    }
+}
