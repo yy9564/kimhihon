ec3f3ba2b1ccfbecdff2d5dc2617f9a0d30a9b0b
doyoung <arktics@naver.com>
2018-04-16 19:13:59 +0900

    Revert "Revert "맵 1층 생성""
    This reverts commit 6e97b047de42e963359f46465e5da9eb69e03784.

--- yeah/Assets/Scripts/CameraControl.cs
+++ yeah/Assets/Scripts/CameraControl.cs
@@ -6,16 +6,26 @@ public class CameraControl : MonoBehaviour {
     public GameObject player;
     //private Vector2 velocity;
     //public float smoothTimeX;
+    public static float rotY;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         //player = GameObject.FindGameObjectWithTag
-	}
+        rotY = gameObject.transform.rotation.eulerAngles.y;
+    }
 	
 	// Update is called once per frame
 	void Update () {
         float posX = player.transform.position.x;
+        rotY = gameObject.transform.rotation.eulerAngles.y;
 
-        transform.position = new Vector3(posX, 0, -10);
-	}
+        //transform.position = new Vector3(posX, 0, -10);
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && (rotY==0)){
+            transform.Rotate(new Vector3(0, 180, 0));
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && (rotY == 180)) {
+            transform.Rotate(new Vector3(0,-180,0));
+        }
+    }
 }
--- yeah/Assets/Scripts/PlayerMoveScene.cs
+++ yeah/Assets/Scripts/PlayerMoveScene.cs
@@ -4,25 +4,49 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerMoveScene : MonoBehaviour {
+    public static float posX;
+    public static float posZ;
+    public static string state = null;
 
-	// Use this for initialization
-	void Start () {
-		
+    // Use this for initialization
+    void Start () {
+        transform.position = new Vector3(posX, 0, posZ);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
-    private void OnTriggerEnter2D(Collider2D collision) {
-        
+        posX = transform.position.x;
+        posZ = transform.position.z;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            transform.position = new Vector3(posX, -3.351106f, -5);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            transform.position = new Vector3(posX, -3.351106f, 5);
+        }
     }
+
     private void OnTriggerStay2D(Collider2D collision) {
         if (collision.gameObject.tag == "Door") {
-            if (Input.GetKey(KeyCode.UpArrow)) {
+            if (Input.GetKeyDown(KeyCode.UpArrow)) {
                 SceneManager.LoadScene("_Complete-Game");
             }
-
+        }
+        if (collision.gameObject.tag == "GO") {
+            if (Input.GetKey(KeyCode.UpArrow)) {
+                int SceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+                if (SceneManager.sceneCountInBuildSettings > SceneIndex) {
+                    SceneManager.LoadScene(SceneIndex);
+                }
+            }
+        }
+        if (collision.gameObject.tag == "BACK") {
+            if (Input.GetKey(KeyCode.UpArrow)) {
+                int SceneIndex = SceneManager.GetActiveScene().buildIndex - 1;
+                if (SceneManager.sceneCountInBuildSettings > SceneIndex) {
+                    SceneManager.LoadScene(SceneIndex);
+                }
+            }
         }
     }
 }
