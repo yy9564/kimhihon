6e97b047de42e963359f46465e5da9eb69e03784
doyoung <arktics@naver.com>
2018-04-16 18:56:10 +0900

    Revert "맵 1층 생성"
    This reverts commit 1c2c40172953dd57433d0fe9813321e9f925c3d7.

--- yeah/Assets/Scripts/PlayerMoveScene.cs
+++ yeah/Assets/Scripts/PlayerMoveScene.cs
@@ -4,49 +4,25 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerMoveScene : MonoBehaviour {
-    public static float posX;
-    public static float posZ;
-    public static string state = null;
 
-    // Use this for initialization
-    void Start () {
-        transform.position = new Vector3(posX, 0, posZ);
+	// Use this for initialization
+	void Start () {
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        posX = transform.position.x;
-        posZ = transform.position.z;
-
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-            transform.position = new Vector3(posX, -3.351106f, -5);
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow)) {
-            transform.position = new Vector3(posX, -3.351106f, 5);
-        }
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision) {
+        
     }
-
     private void OnTriggerStay2D(Collider2D collision) {
         if (collision.gameObject.tag == "Door") {
-            if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            if (Input.GetKey(KeyCode.UpArrow)) {
                 SceneManager.LoadScene("_Complete-Game");
             }
-        }
-        if (collision.gameObject.tag == "GO") {
-            if (Input.GetKey(KeyCode.UpArrow)) {
-                int SceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
-                if (SceneManager.sceneCountInBuildSettings > SceneIndex) {
-                    SceneManager.LoadScene(SceneIndex);
-                }
-            }
-        }
-        if (collision.gameObject.tag == "BACK") {
-            if (Input.GetKey(KeyCode.UpArrow)) {
-                int SceneIndex = SceneManager.GetActiveScene().buildIndex - 1;
-                if (SceneManager.sceneCountInBuildSettings > SceneIndex) {
-                    SceneManager.LoadScene(SceneIndex);
-                }
-            }
+
         }
     }
 }
