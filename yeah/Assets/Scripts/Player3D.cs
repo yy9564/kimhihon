36d358e5a7e53c556c72911636a2f63112e1d528
doyoung <arktics@naver.com>
2018-04-18 14:14:52 +0900

    맵 수정, 2층 생성

---
+++ yeah/Assets/Scripts/Player3D.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player3D : MonoBehaviour {
+    float h, v;
+    public float moveSpeed = 0.1f;
+    public float rotateSpeed = 100.0f;
+    Transform tr;
+
+    // Use this for initialization
+    void Start () {
+        tr = GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        Vector3 moveDir;
+
+        moveDir = (Vector3.forward * v) + (Vector3.right * h);
+        moveDir.Normalize();
+        tr.Translate(moveDir * moveSpeed * Time.deltaTime);
+        //tr.Rotate(Vector3.up * rotateSpeed * Time.deltaTime * Input.GetAxis("Mouse X"));
+
+    }
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.tag == "Move")
+        {
+            Debug.Log("collision");
+            SceneManager.LoadScene("mainHall2-1");
+        }
+    }
+}
